da6fec605ca3404d0d81d30ae7227a5b74ff51aa
bao1405 <lqbbiker15052@gmail.com>
2024-01-17 14:14:09 +0700

    update file

--- bai4.js
+++ bai4.js
@@ -1,4 +1,4 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 let evenArray = arr.filter(function (element) {
     return element % 2 === 0;
 });
